3e9d96c000e0480cb36b7e62f91b7ea1e522c115
Ishani <ishaniamin@gmail.com>
2018-06-18 20:39:58 +0000

    automatically backed up by learn

--- lib/curfew_checker.rb
+++ lib/curfew_checker.rb
@@ -1,11 +1,9 @@
 def simple_curfew_checker(time)
-
   if time < 11
     puts "Keep having fun!"
   else
     puts "You're in trouble! Better get home quick!"
   end
-
 end
 
 def curfew_checker(time)
